4a0fd4350c40b4b45c785e41e3386c2dde230a40
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-03 23:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ describe('example to-do app', () => {
     it('drag & drop works correctly', () => {
         cy.visit('http://localhost:3000'); // Update with actual URL
 
-        // Select the first `.items` element and perform drag-and-drop
+        // Select first `.items` and perform drag & drop
         cy.get('.items').first()
-            .trigger('mousedown', { which: 1 }) // Start dragging
-            .trigger('mousemove', { clientX: 300, clientY: 300 }) // Move item
-            .trigger('mouseup'); // Drop item
+            .trigger('mousedown', { which: 1 })
+            .trigger('mousemove', { clientX: 300, clientY: 300 })
+            .trigger('mouseup');
 
-        // Ensure that the position of the first `.items` element has changed
+        // Ensure the position of the first item changed
         cy.get('.items').first().should('have.css', 'left');
     });
 });
